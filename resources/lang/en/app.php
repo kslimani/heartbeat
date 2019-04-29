babde240cf707959ec65644aa91d51a89614a873
Karim Slimani <karim@playmedia.fr>
2019-04-29 18:57:19 +0200

    feat: add application language file

---
+++ resources/lang/en/app.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by application.
+    |
+    */
+
+    'toggle_nav' => 'Toggle navigation',
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
+
+    'add' => 'Create',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+
+    'users' => 'Users',
+
+    'name' => 'Name',
+    'email' => 'Email',
+
+];
