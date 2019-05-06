aa97c08ee8abde73ed4479fa8d6307feb6b2fd32
Karim Slimani <karim@playmedia.fr>
2019-05-06 13:21:24 +0200

    feat: render api error in JSON

--- app/Exceptions/ApiExceptionRenderer.php
+++ app/Exceptions/ApiExceptionRenderer.php
@@ -11,28 +11,65 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ApiExceptionRenderer
 {
+    public static function errorResponse($code, $message = null)
+    {
+        switch ($code) {
+            case 403:
+                $message = 'Forbidden';
+                break;
+
+            case 404:
+                $message = 'Not Found';
+                break;
+
+            case 405:
+                $message = 'Method Not Allowed';
+                break;
+
+            case 422:
+                $message = 'Invalid Data';
+                break;
+
+            case 500:
+                $message = 'Unexpected Error';
+                break;
+
+            default:
+                $message = $message ?: 'Unknown Error';
+                break;
+        }
+
+        return response()->json(['error' => $message], $code);
+    }
+
     public static function render($request, Exception $exception)
     {
         if ($exception instanceof AuthenticationException) {
-            return response('', 403);
+
+            return self::errorResponse(403);
         }
 
         if ($exception instanceof AuthorizationException) {
-            return response('', 403);
+
+            return self::errorResponse(403);
         }
 
         if ($exception instanceof ModelNotFoundException) {
-            return response('', 404);
-        }
 
-        if ($exception instanceof ValidationException) {
-            return response('', 422);
+            return self::errorResponse(404);
         }
 
         if ($exception instanceof HttpException) {
-            return response('', $exception->getStatusCode());
+
+            return self::errorResponse($exception->getStatusCode());
         }
 
-        return response('', 400);
+        if ($exception instanceof ValidationException) {
+
+            // FIXME: expose validation error messages ?
+            return self::errorResponse(422);
+        }
+
+        return self::errorResponse(500);
     }
 }
