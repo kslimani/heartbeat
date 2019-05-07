6c8e373c41df33061085fad6a816f007d23a6731
Karim Slimani <karim@playmedia.fr>
2019-05-07 17:12:45 +0200

    test: add helper methods in testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Support\AppInstaller;
+use App\Role;
+use App\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function installApplication($email, $password)
+    {
+        (new AppInstaller)->install($email, $password);
+    }
+
+    public function createAdminUser(array $attributes = [])
+    {
+        $user = factory(User::class)->create($attributes);
+
+        Role::admin()->users()->attach([$user->id]);
+
+        return $user;
+    }
 }
