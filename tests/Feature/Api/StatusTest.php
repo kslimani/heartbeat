6fa699f269d06f63d9293c94c8fd47ad571077af
Karim Slimani <karim@playmedia.fr>
2019-05-07 17:16:17 +0200

    test(status): add device service status assertions

--- tests/Feature/Api/StatusTest.php
+++ tests/Feature/Api/StatusTest.php
@@ -11,6 +11,12 @@ class StatusTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->installApplication('admin@test.com', 'secret123');
+    }
+
     public function testItCheck()
     {
         $user = factory(User::class)->create();
@@ -30,6 +36,48 @@ class StatusTest extends TestCase
 
         $response = $this->json('POST', '/api/status/check', [
             'key' => 'does-not-exists-in-database-39-str-size',
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function testItHandleServiceStatus()
+    {
+        $user = factory(User::class)->create();
+        $key = KeyGenerator::make($user);
+
+        $response = $this->json('POST', '/api/status', [
+            'key' => $key->data,
+            'device' => 'my.device',
+            'service' => 'acme-srv',
+            'status' => 'UP',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->json('POST', '/api/status', [
+            'key' => $key->data,
+            'device' => 'my.device',
+            'service' => 'bad@name',
+            'status' => 'UP',
+        ]);
+
+        $response->assertStatus(422);
+
+        $response = $this->json('POST', '/api/status', [
+            'key' => $key->data,
+            'device' => 'my.device',
+            'service' => 'acme-srv',
+            'status' => 'UNKNOW_STATUS',
+        ]);
+
+        $response->assertStatus(400);
+
+        $response = $this->json('POST', '/api/status', [
+            'key' => 'bad.key',
+            'device' => 'my.device',
+            'service' => 'acme-srv',
+            'status' => 'UP',
         ]);
 
         $response->assertStatus(403);
