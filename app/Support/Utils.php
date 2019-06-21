c86037b385badb4913703684c0bae02869b9ee6c
Karim Slimani <karim@playmedia.fr>
2019-06-21 16:15:16 +0200

    feat(utils): add global devices mute

--- app/Support/Utils.php
+++ app/Support/Utils.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Carbon;
 
 class Utils
 {
+    const ALL_DEVICES_MUTED = '_all_devices_muted';
+
     /**
      * Get elapsed duration in seconds
      *
@@ -17,5 +19,31 @@ class Utils
         $elapsed = Carbon::now()->timestamp - $date->timestamp;
 
         return $elapsed > 0 ? $elapsed : 0;
+    }
+
+    /**
+     * Mute all devices
+     */
+    public static function muteAllDevices()
+    {
+        AppStore::put(self::ALL_DEVICES_MUTED, true);
+    }
+
+    /**
+     * Unmute all devices
+     */
+    public static function unmuteAllDevices()
+    {
+        AppStore::put(self::ALL_DEVICES_MUTED, false);
+    }
+
+    /**
+     * Check if all devices are muted
+     *
+     * @return bool
+     */
+    public static function allDeviceMuted()
+    {
+        return AppStore::get(self::ALL_DEVICES_MUTED) === true;
     }
 }
