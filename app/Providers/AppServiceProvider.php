ffc905d2c51655d3a793459abaf3f46ceba43398
Karim Slimani <karim@playmedia.fr>
2019-06-28 17:51:09 +0200

    feat: setup locale in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Support\Locale;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Locale::boot([
+            'aliases' => [
+                'en' => 'en_US.utf8',
+                'fr' => 'fr_FR.utf8',
+            ],
+            'iso_formats' => [
+                'en' => [
+                    Locale::TYPE_DATETIME => 'dddd, MMMM D, YYYY - h:mm:ss A', // Default is "dddd, MMMM D, YYYY h:mm A"
+                ],
+                'fr' => [
+                    Locale::TYPE_DATETIME => 'dddd D MMMM YYYY - HH:mm:ss', // Default is "dddd D MMMM YYYY HH:mm"
+                ],
+            ],
+        ]);
     }
 }
