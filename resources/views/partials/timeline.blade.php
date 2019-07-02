9f423815d3efa85fbdb1243df89137d84a1bd39e
Karim Slimani <karim@playmedia.fr>
2019-07-02 12:40:18 +0200

    feat: add param in timeline partial view

--- resources/views/partials/timeline.blade.php
+++ resources/views/partials/timeline.blade.php
@@ -5,7 +5,9 @@
                 <div class="timeline mdi mdi-24px {{ $event->toStatus->name === 'UP' ? 'mdi-check-circle text-success' : 'mdi-alert-circle text-danger' }}"></div>
                 <div class="col">
                     <div class="timeline-date">{{ $event->label_date }}</div>
-                    <div><a href="#">{{ $event->serviceStatus->service->label }}</a> @ {{ $event->serviceStatus->device->label }}</div>
+                    @unless (isset($withoutDeviceLine))
+                    <div><a href="{{ route('service-events.show', ['id' => $event->serviceStatus->id]) }}">{{ $event->serviceStatus->service->label }}</a> @ {{ $event->serviceStatus->device->label }}</div>
+                    @endunless
                     <div>
                         {{ __('app.status_from') }}<span class="badge">{{ $event->fromStatus->name }}</span>{{ __('app.status_to') }}<span class="badge">{{ $event->toStatus->name }}</span>
                     </div>
