c1eb12fc14980eedf8fa7be57f3ebdc1bc859d0f
Karim Slimani <karim@playmedia.fr>
2019-07-09 16:38:34 +0200

    fix: add user account missing translations

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -126,7 +126,7 @@
                 <input type="hidden" name="tab" value="security">
 
                 <div class="form-group row">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('app.password') }}</label>
 
                     <div class="col-md-6">
                         <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
@@ -140,7 +140,7 @@
                 </div>
 
                 <div class="form-group row">
-                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('app.password_confirm') }}</label>
 
                     <div class="col-md-6">
                         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
